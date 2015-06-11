17d0c73f22a55175019ce9e42d5d5453a3d545e6
Marius Lueck <mluecksen@gmail.com>
2015-06-11 23:09:01 +0200

    woops

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -2,6 +2,4 @@ import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
 
-console.log(specs);
-
 Router.run(routes, Handler => React.render(<Handler />, document.body));
