45b4c4427301da679c9853725886b4f69bebd26e
xmyno <xmynox@gmail.com>
2015-06-12 08:57:35 +0200

    implemented add socket action

--- backend/actions/socket.js
+++ backend/actions/socket.js
@@ -3,10 +3,13 @@
 var Raid = require('../models/raid');
 
 var Actions = {
-  processMessage: function (data) {
+  processMessage: function (data, socketResponse) {
+    console.log(data);
+    if (!data.action && !data.compId && !data.user) return;
+
     switch(data.action) {
       case 'add':
-
+        if (data.character) this.addCharacter(data, 0, socketResponse);
         break;
       case 'move':
 
@@ -14,7 +17,64 @@ var Actions = {
       case 'remove':
 
         break;
+      case 'name':
+
+        break;
     }
+  },
+  addCharacter: function (data, raidId, socketResponse) {
+    var character = data.character;
+    if (!character.name || !character.realm || !character.region ||
+      !character.className || !character.spec || !character.role) {
+
+      socketResponse(data.compId + ':' + data.user, { error: 'Some character data was missing.' });
+      return;
+    }
+    if (!raidId >= 0) {
+      socketResponse(data.compId + ':' + data.user, { error: 'Bad raid id given.' });
+      return;
+    }
+
+    Raid.findOne({ _compId: data.compId, id: raidId })
+        .exec(function (err, raid) {
+          if (err) return handleError(err);
+          if (!raid) {
+            socketResponse(data.compId + ':' + data.user, { error: 'Specified RaidComp not found.' });
+            return;
+          }
+
+          for (var raidchar of raid.characters) {
+            if (raidchar.name === character.name
+              && raidchar.realm === character.realm
+              && raidchar.region === character.region) {
+
+              raidchar.className = character.className;
+              raidchar.spec = character.spec;
+              raidchar.role = character.role;
+
+              raid.save(function (err) {
+                if (err) return handleError(err);
+                socketResponse(data.compId + ':' + data.user, { msg: 'update' });
+              });
+              return;
+            }
+          }
+
+          // add a new character
+          raid.characters.push({
+            name: character.name,
+            realm: character.realm,
+            region: character.region,
+            className: character.className,
+            spec: character.spec,
+            role: character.role
+          });
+          raid.save(function (err) {
+            if (err) return handleError(err);
+            socketResponse(data.compId, { msg: 'added' + character.name });
+          });
+          return;
+    });
   }
 }
 
--- backend/app.js
+++ backend/app.js
@@ -20,14 +20,16 @@ var router = require('./routes/api');
 app.use('/comp', router);
 
 // debug
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/client.html');
-// });
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client.html');
+});
 
 var SocketActions = require('./actions/socket');
 io.on('connection', function (socket) {
   socket.on('raidcomp', function (data) {
-    SocketActions.processMessage(data);
+    SocketActions.processMessage(data, function(socket, result) {
+      io.emit(socket, result);
+    });
   });
 });
 
