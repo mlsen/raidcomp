1d9c2250608f1e5ee30b730c6f5b9cb318e3f2f3
Marius Lueck <mluecksen@gmail.com>
2015-06-16 23:29:40 +0200

    fetch not available in firefox

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
+import 'whatwg-fetch';
 
 Router.run(routes, Handler => {
   React.render(<Handler />, document.getElementById('app'))
