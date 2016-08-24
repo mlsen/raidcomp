7552c1125743f2c86f619db83b05501065ae2b9a
xmyno <xmynox@gmail.com>
2016-08-24 19:16:10 +0200

    Use url from config

--- frontend/app/scripts/actions/AppActions.js
+++ frontend/app/scripts/actions/AppActions.js
@@ -1,12 +1,13 @@
 import alt from '../alt.jsx';
 import io from 'socket.io-client';
+import config from '../config.jsx';
 
 
 class AppActions {
 
   connectToSocket() {
     console.log("connecting to socket");
-    return io.connect('http://api.raidcomp.dev');
+    return io.connect(config.baseUrl);
   }
 
 }
